4da37782dd50e12c99b9e9de03080d683ebb0f57
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-02-13 13:53:20 +0530

    on signup screen

--- Bloom/ViewController.swift
+++ Bloom/ViewController.swift
@@ -13,16 +13,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        signUpButton.layer.borderWidth = 2
-        signUpButton.layer.borderColor = UIColor.black.cgColor
-        
-        signUpButton.layer.shadowOpacity = 0.5
-        signUpButton.layer.shadowColor = UIColor.black.cgColor
-        signUpButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-        signUpButton.layer.shadowRadius = 2.0
-
-        
+//
+//        signUpButton.layer.borderWidth = 2
+//        signUpButton.layer.borderColor = UIColor.black.cgColor
+//
+//        signUpButton.layer.shadowOpacity = 0.5
+//        signUpButton.layer.shadowColor = UIColor.black.cgColor
+//        signUpButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+//        signUpButton.layer.shadowRadius = 2.0
+//
+//
        
     }
    
